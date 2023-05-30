3a91753929d5f4fda09679912cebc1800af69051
atounaok <atounaok@gmail.com>
2023-05-29 21:02:13 -0400

    :hammer: Signin avec gitgub

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -4,6 +4,8 @@ import Input from '@/components/Input'
 import axios from 'axios';
 import { signIn } from 'next-auth/react'
 import Router, { useRouter } from 'next/router';
+import { FcGoogle } from 'react-icons/fc'
+import { FaGithub } from 'react-icons/fa'
 
 const Auth = () => {
     const router = useRouter();
@@ -87,13 +89,44 @@ const Auth = () => {
                     {variant === 'login' ? 'Login' : 'Sign up'}
                 </button>
 
+                <div className='flex flex-row items-center gap-4 mt-8 justify-center'>
+                    <div className='
+                        w-10
+                        h-10
+                        bg-white
+                        rounded-full
+                        flex
+                        items-center
+                        justify-center
+                        cursor-pointer
+                        hover:opacity-80
+                        transition
+                    '>
+                        <FcGoogle size={30}/>
+                    </div>
+                    <div onClick={() => signIn('github', { callbackUrl: '/' })}
+                    className='
+                        w-10
+                        h-10
+                        bg-white
+                        rounded-full
+                        flex
+                        items-center
+                        justify-center
+                        cursor-pointer
+                        hover:opacity-80
+                        transition
+                    '>
+                        <FaGithub size={30}/>
+                    </div>
+                </div>
+
                 <p className='text-neutral-500 mt-12'>
                     {variant === 'login' ? 'First time using Netflix?' : 'Already have an account?'}  
                     <span onClick={toggleVariant} className='text-white ml-1 hover:underline hover:text-gray-200 cursor-pointer'>
                         {variant === 'login' ? 'Create an account' : 'Login'}    
                     </span>    
                 </p>
-
             </div>
         </div>
       </div>
