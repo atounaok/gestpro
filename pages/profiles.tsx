26134f84e396a0b3bf41bc54dfb5d75bc602a003
atounaok <atounaok@gmail.com>
2023-05-30 01:06:59 -0400

    :hammer: Style page profile

--- pages/profiles.tsx
+++ pages/profiles.tsx
@@ -4,6 +4,7 @@ import { NextPageContext } from 'next'
 import { getSession } from 'next-auth/react'
 import React from 'react'
 import Image from 'next/image';
+import Link from 'next/link';
 
 export async function getServerSideProps(context: NextPageContext) {
     const session = await getSession(context);
@@ -24,10 +25,38 @@ export async function getServerSideProps(context: NextPageContext) {
 
 const Profiles = () => {
     const { data: user } = useCurrentUser();
+    const infosProfile = [
+        "Infos personnelles",
+        "Mot de passe"
+    ]
   return (
-    <div>
-        <p>Page de profile : {user?.name}</p>
-        <Image src={user?.image} width={30} height={30} alt='user image' className='object-contain'/>
+    <div className='h-screen'>
+        <div className='h-[35%] bg-[#141414] flex flex-col justify-center items-center'>
+            <Image src={user?.image} width={80} height={80} alt='user image' 
+            className='
+                object-contain
+                rounded-full
+                mb-2
+                shadow
+                md:w-[130px]
+                '/>
+            <p className='text-[#f9f9f9] text-lg md:text-3xl'>{user?.email}</p>
+        </div>
+        <div className='bg-[#141414] flex justify-center'>
+        <ul className='flex flex-row w-[60%] justify-between items-baseline'>
+                {
+                    infosProfile.map((info, index) => {
+                        return (
+                            <div key={index} className="text-[#f9f9f9]">
+                                <Link href="" className='font-thin text-sm hover:border-b'>
+                                    {info}
+                                </Link>
+                            </div>
+                        )
+                    })
+                }
+            </ul>
+        </div>
     </div>
   )
 }
