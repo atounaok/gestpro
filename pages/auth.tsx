872885ce9e66025772f577c4f8d3ddc538977fb2
atounaok <atounaok@gmail.com>
2023-05-29 20:37:11 -0400

    :hammer: Créer compte good

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,9 +1,104 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
+import Image from 'next/image'
+import Input from '@/components/Input'
+import axios from 'axios';
+import { signIn } from 'next-auth/react'
+import Router, { useRouter } from 'next/router';
 
-const auth = () => {
+const Auth = () => {
+    const router = useRouter();
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [variant, setVariant] = useState('login');
+
+    const toggleVariant = useCallback(() => {
+        setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login')
+    }, [])
+
+
+    // Login
+    const login = useCallback(async () => {
+      try {
+          await signIn('credentials', {
+              email,
+              password,
+              redirect: false,
+              callbackUrl: '/'
+          });
+
+          router.push('/');
+      } catch (error) {
+          console.log(error)
+      }
+  }, [email, password, router])
+  
+  // Register
+  const register = useCallback(async () => {
+      try {
+          await axios.post('/api/register', {
+              email,
+              name,
+              password
+          });
+
+          login()
+      } catch (error) {
+          console.log(error)
+      }
+  }, [email, name, password, login]);
+
+
   return (
-    <div>auth</div>
+    <div className="">
+      <div className="">
+        <div className='flex justify-center'>
+            <div className='px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full'>
+                <h2 className='text-white text-4xl mb-8 font-semibold'>
+                    {variant === 'login' ? 'Sign in' : 'Register'}
+                </h2>
+                <div className='flex flex-col gap-4'>
+                    {variant === 'register' && (
+                    <Input
+                        label='Username'
+                        onChange={(e:any) => setName(e.target.value)}
+                        id="name"
+                        value={name}
+                    />)}
+
+                    <Input
+                        label='Email'
+                        onChange={(e:any) => setEmail(e.target.value)}
+                        id="email"
+                        type="email"
+                        value={email}
+                    />
+                    <Input
+                        label='Password'
+                        onChange={(e:any) => setPassword(e.target.value)}
+                        id="password"
+                        type="password"
+                        value={password}
+                    />
+                </div>
+
+                <button onClick={variant === 'login' ? login : register} className='bg-gray-900 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition'>
+                    {variant === 'login' ? 'Login' : 'Sign up'}
+                </button>
+
+                <p className='text-neutral-500 mt-12'>
+                    {variant === 'login' ? 'First time using Netflix?' : 'Already have an account?'}  
+                    <span onClick={toggleVariant} className='text-white ml-1 hover:underline hover:text-gray-200 cursor-pointer'>
+                        {variant === 'login' ? 'Create an account' : 'Login'}    
+                    </span>    
+                </p>
+
+            </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default auth
+export default Auth
