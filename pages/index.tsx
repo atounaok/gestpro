faf991c247785bdef7132f6728569e6a44eacb85
atounaok <atounaok@gmail.com>
2023-05-29 21:52:38 -0400

    :hammer: Available session

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,25 @@
+import { NextPageContext } from 'next'
+import { getSession } from 'next-auth/react'
 import React from 'react'
+
+export async function getServerSideProps(context: NextPageContext){
+  const session = await getSession(context)
+
+  if(!session){
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      }
+    }
+  }
+
+  return {
+    props: {
+      
+    }
+  }
+}
 
 const Home = () => {
   return (
