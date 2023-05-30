7d9c33da9f46d0bbf9c8e3f82d533d1429a43721
atounaok <atounaok@gmail.com>
2023-05-29 23:09:50 -0400

    :hammer: Page profile affichage de l'image du user

--- pages/profiles.tsx
+++ pages/profiles.tsx
@@ -1,6 +1,9 @@
+import useCurrentUser from '@hooks/useCurrentUser';
+import { Session } from 'inspector';
 import { NextPageContext } from 'next'
 import { getSession } from 'next-auth/react'
 import React from 'react'
+import Image from 'next/image';
 
 export async function getServerSideProps(context: NextPageContext) {
     const session = await getSession(context);
@@ -20,9 +23,11 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 const Profiles = () => {
+    const { data: user } = useCurrentUser();
   return (
     <div>
-        Page de profile
+        <p>Page de profile : {user?.name}</p>
+        <Image src={user?.image} width={30} height={30} alt='user image' className='object-contain'/>
     </div>
   )
 }
