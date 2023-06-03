b607f3f9ce1e6bec6bc73fa4cb8104ae1401581c
atounaok <atounaok@gmail.com>
2023-06-02 21:54:35 -0400

    :hammer: Avancements

--- components/Task.tsx
+++ components/Task.tsx
@@ -7,7 +7,9 @@ const Task = ({ title, totalItems, completedItems }: { title: string, totalItems
       <li className='border shadow bg-[#f9f9f9] cursor-pointer hover:bg-gray-100 p-2 rounded-md'>
         <div className='flex justify-between items-center mb-2'>
           <p className='font-light text-sm'>{title}</p>
-          <TfiPencil className='text-gray-500 cursor-pointer hover:text-gray-400'/>
+          <button className='hover:bg-gray-200 p-1 rounded-md' onClick={() => alert('Modifier task')}>
+            <TfiPencil className='text-gray-500 text-md'/>
+          </button>
         </div>
         <div className='flex justify-start items-center px-1'>
           <BsTextParagraph className='me-3 text-sm font-thin'/>
